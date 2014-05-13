1bd43cf93a17b8f58ed19d3e44434cd47f4e44f2
D Win <dwin@wintech.net.nz>
2014-05-13 22:41:41 +1200

    Code Review Finished
    Tidied up code
    Added Tests that can be performed from cURL
    Storage of hashes now works via POST rather than PUT
    Code commenting! Yay!

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import logging
+import json
 
-from bottle import route, run, response, abort
+from bottle import route, run, response, abort, post, request
 
-api_version = "0.0.2" #API version, please iterate every time you push a version
+api_version = "0.0.3" #API version, please iterate every time you push a version
 hash_storage = {} #storage array for hashes
 
 @route('/')
@@ -12,40 +13,50 @@ def root():
 
 @route('/url/<url>', method='GET')
 def hash_get(url):
+    """Returns all the hashes for a given url.
+
+    Keyword arguments:
+    url -- the url you want the hashes for.
+    """
     try:
-        output = ""
+        output = []
         hash_list = hash_storage[url]
-        for hash in hash_list:
-            output.append({"hash":hash})
+        for hashes in hash_list:
+            output.append(hashes)
         response = {"url":url,"hashes":output}
         return json.dumps(response)
     except KeyError:
         errorOutput = "URL Not in Database"
         logging.warning(errorOutput)
         abort(400, errorOutput)
+        
 
-def outputJSONHashesForURL(url):
-    output = []
-    for hash in a:
-        if(hash[0] == url):
-            output.append( { "success" : True, "URL" : hash[0],  "HASH" : hash[1] } )
-    return output
-    
-@route('/url/<url>/hash/<myhash>', method='PUT')
-def hash_put(myhash, url):
-    if(is_sha_256_hash(myhash) and is_sha_256_hash(url)):
-        hash_storage[url] = (url, myhash)
-        """Either:
-        #response.status = 201
-        #return json.dumps({url: myhash})
-        or:
-        #body_output = json.dumps({url: myhash})
-        #return bottle.HTTPResponse(status=201, body=body_output)
-        """
+@route('/url', method='POST')
+def hash_put():
+    """Stores a given url and hash.
+
+    Data must be posted in JSON format in the form:
+    {url:theurltosubmit,hash:thehashtosubmit}
+    """
+    posted_data = json.loads(request.body.read().decode("utf-8"))
+    posted_url = posted_data['url']
+    posted_hash = posted_data['hash']
+    if(is_sha_256_hash(posted_hash)):
+        if(posted_url in hash_storage):
+            hash_list = hash_storage[posted_url]
+            hash_list.append(posted_hash)
+        else:
+            hash_storage[posted_url] = [posted_hash]
     else:
-        return { "success" : False, "URL" : url,  "HASH" : myhash }
+        abort(400, "SHA256 Hash not submitted!")
     
 def is_sha_256_hash(name):
+    """Form a complex number.
+
+    Keyword arguments:
+    real -- the real part (default 0.0)
+    imag -- the imaginary part (default 0.0)
+    """
     if len(name) == 64:
         return True
     else:
